3544564e791b97af382e71d3b9d9982842410233
Meirkhan <baidullameirkhan78@gmail.com>
2026-01-26 21:40:39 +0500

    new example

---
+++ practice 1/syntax.py
@@ -0,0 +1,24 @@
+if 5 > 2:
+  print("Five is greater than two!")
+
+
+#syntaxerror
+if 5 > 2:
+print("Five is greater than two!")
+
+
+if 5 > 2:
+ print("Five is greater than two!") 
+if 5 > 2:
+        print("Five is greater than two!") 
+
+
+
+#syntaxerror
+if 5 > 2:
+ print("Five is greater than two!")
+        print("Five is greater than two!")
+
+
+
+print("Python is fun!")
