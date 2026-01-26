519578ee4312717a4ef89bf7277568513b0a4132
Meirkhan <baidullameirkhan78@gmail.com>
2026-01-26 21:40:14 +0500

    new example

---
+++ practice 1/strings.py
@@ -0,0 +1,28 @@
+a = "Hello"
+print(a)
+
+
+
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
+
+
+
+a = '''Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua.'''
+print(a)
+
+
+
+a = "Hello, World!"
+print(a[1])
+
+
+
+txt = "The best things in life are free!"
+print("free" in txt)
