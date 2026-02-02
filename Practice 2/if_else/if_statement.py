43d9a4ef54094016978d30ced363fbdfced67036
baidullameirkhan78-lab <baidullameirkhan78@gmail.com>
2026-02-02 09:26:51 +0500

    Update if_statement.py

---
+++ Practice 2/if_else/if_statement.py
@@ -0,0 +1,48 @@
+# 1. Баға бойынша баға
+score = 85
+if score >= 90:
+    print("A")
+elif score >= 75:
+    print("B")
+else:
+    print("C")
+
+
+# 2. Температура
+temp = 10
+if temp > 30:
+    print("Hot")
+elif temp > 15:
+    print("Warm")
+else:
+    print("Cold")
+
+
+# 3. Санның белгісі
+x = 0
+if x > 0:
+    print("Positive")
+elif x < 0:
+    print("Negative")
+else:
+    print("Zero")
+
+
+# 4. Жас категориясы
+age = 12
+if age >= 18:
+    print("Adult")
+elif age >= 13:
+    print("Teen")
+else:
+    print("Child")
+
+
+# 5. Кіру уақыты
+hour = 22
+if hour < 12:
+    print("Morning")
+elif hour < 18:
+    print("Day")
+else:
+    print("Evening")
