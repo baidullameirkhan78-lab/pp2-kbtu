4e510af6315f932bddb3925bb76ab8a6fc9dda7c
baidullameirkhan78-lab <baidullameirkhan78@gmail.com>
2026-02-02 09:27:04 +0500

    Add short-hand if examples in Python

---
+++ Practice 2/if_else/short_hand_if.py
@@ -0,0 +1,24 @@
+# 1. Қарапайым short if
+age = 20
+print("Adult") if age >= 18 else print("Minor")
+
+
+# 2. Жұп / тақ
+n = 3
+print("Even") if n % 2 == 0 else print("Odd")
+
+
+# 3. Пароль
+password = "1234"
+print("OK") if password == "1234" else print("Wrong")
+
+
+# 4. Boolean
+is_student = True
+print("Student") if is_student else print("Not student")
+
+
+# 5. Салыстыру
+x = 5
+y = 8
+print("x is bigger") if x > y else print("y is bigger")
