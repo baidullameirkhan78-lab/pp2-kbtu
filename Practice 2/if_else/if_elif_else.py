6a33ae0cbea347c0603ec5447894b180f4a3c229
baidullameirkhan78-lab <baidullameirkhan78@gmail.com>
2026-02-02 09:25:56 +0500

    Update if_elif_else.py

---
+++ Practice 2/if_else/if_elif_else.py
@@ -0,0 +1,43 @@
+# 1. Жас тексеру
+age = 18
+
+if age >= 18:
+    print("Adult")
+else:
+    print("Child")
+
+
+# 2. Сан оң ба, теріс пе
+x = -5
+
+if x > 0:
+    print("Positive")
+else:
+    print("Not positive")
+
+
+# 3. Жұп санды тексеру
+number = 7
+
+if number % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
+
+
+# 4. Пароль тексеру
+password = "1234"
+
+if password == "1234":
+    print("Access granted")
+else:
+    print("Access denied")
+
+
+# 5. Boolean айнымалымен if
+is_student = True
+
+if is_student:
+    print("Student")
+else:
+    print("Not student")
