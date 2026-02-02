f0f29bf27c8a491878951035d5c42ca7958a6ea0
baidullameirkhan78-lab <baidullameirkhan78@gmail.com>
2026-02-02 10:36:13 +0500

    Update while_loop.py

---
+++ pp2-kbtu/Practice 2/loops/while_loop.py
@@ -0,0 +1,36 @@
+# 1. 1-ден 5-ке дейін
+i = 1
+while i <= 5:
+    print(i)
+    i += 1
+
+
+# 2. 0-ден 4-ке дейін
+x = 0
+while x < 5:
+    print(x)
+    x += 1
+
+
+# 3. Countdown
+n = 3
+while n > 0:
+    print(n)
+    n -= 1
+
+
+# 4. Even numbers
+i = 2
+while i <= 10:
+    print(i)
+    i += 2
+
+
+# 5. Boolean while
+is_on = True
+count = 0
+while is_on:
+    print("Running")
+    count += 1
+    if count == 3:
+        is_on = False
